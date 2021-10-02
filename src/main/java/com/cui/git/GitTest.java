ce1928c792e684a4b07c80617842afd81cac736e
cuijp <15735941285@163.com>
2021-10-02 18:03:42 +0800

    first commit

---
+++ src/main/java/com/cui/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.cui.git;
+
+/**
+ * @author cuijp
+ * @version 1.0
+ * @description：
+ * @Date 2021/10/2 17:58
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
